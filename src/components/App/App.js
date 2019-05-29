c1d37b195ffff8bade35d8c386f73de47fddda74
kobesparrow <markcollins.md@gmail.com>
2019-05-29 10:42:44 -0600

    Setup file structure

---
+++ src/components/App/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <section className="header-links">
+          <p>HOME</p>
+          <p>ABOUT</p>
+          <p>WORK</p>
+          <p>RESUME</p>
+          <p>CONTACT</p>
+        </section>
+        <section className="social-links">
+          <p>social</p>
+        </section>
+      </header>
+    </div>
+  );
+}
+
+export default App;
