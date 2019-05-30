7fda3d8fb44c70fcd927cf900230dfec77564d8a
kobesparrow <markcollins.md@gmail.com>
2019-05-30 15:04:54 -0600

    Text styles

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -5,10 +5,12 @@ const Project = ({title, stack, deets, goals, image}) => {
   return(
     <article className='project'>
       <img src={ image } className='project-image' />
-      <p>{ title }</p>
-      <p>{ stack }</p>
-      <p>{ deets }</p>
-      <p>{ goals }</p>
+      <section className='project-details'>
+        <p className='project-title'>{ title }</p>
+        <p className='tech-stack'><span className='project-subhead'>Tech Stack: </span>{ stack }</p>
+        <p>{ deets }</p>
+        <p>{ goals }</p>
+      </section>
     </article>
   )
 }
--- src/utils/project-data.js
+++ src/utils/project-data.js
@@ -1,30 +1,30 @@
 export const projectData = [
   { id: 1,
-    title: 'Project 1',
-    stack: 'React, SCSS',
-    deets: 'Info About the App',
-    goals: 'Learn how to use React',
+    title: 'Talking Pictures',
+    stack: 'React, Router, Redux, Sass, Jest/Enzyme, PostgreSQL',
+    deets: `Represents the user's movie collection, allowing them to view current and upcoming films, login, and save favorites`,
+    goals: 'Incorporated Redux and nuances of fetching data and storing it locally or globally with an emphasis on React frameworks',
     image: 'https://i.postimg.cc/GmxWdvzd/Screen-Shot-2019-05-14-at-11-05-34-PM.png'
   },
   { id: 2,
-    title: 'Project 2',
-    stack: 'React, SCSS',
-    deets: 'Info About the App',
-    goals: 'Learn how to use React',
-    image: 'https://i.postimg.cc/GmxWdvzd/Screen-Shot-2019-05-14-at-11-05-34-PM.png'
+    title: 'Board, Not Bored',
+    stack: 'React, Sass, Jest/Enzyme',
+    deets: 'Encourages local board gaming by helping gamers select a game then displaying nearby establishments which offer said game',
+    goals: 'Passion project which has tested individual problem-solving ability and incoporates user feedback to continually add features',
+    image: 'https://i.postimg.cc/G3xv3B7P/Screen-Shot-2019-05-30-at-10-56-30-AM.png'
   },
   { id: 3,
-    title: 'Project 3',
-    stack: 'React, SCSS',
-    deets: 'Info About the App',
-    goals: 'Learn how to use React',
-    image: 'https://i.postimg.cc/GmxWdvzd/Screen-Shot-2019-05-14-at-11-05-34-PM.png'
+    title: `Oh, The Styles You'll Code`,
+    stack: 'React, Sass, Jest/Enzyme',
+    deets: 'A Dr. Seuss inspired app for Sass terminology',
+    goals: 'Built in five days, represents a strong grasp of React concepts with a commitment to to style and user experience',
+    image: 'https://i.postimg.cc/8CXn0KW5/Screen-Shot-2019-04-17-at-1-32-34-PM.png'
   },
   { id: 4,
-    title: 'Hannah is great',
-    stack: 'React, SCSS',
-    deets: 'Info About the App',
-    goals: 'Learn how to use React',
-    image: 'https://i.postimg.cc/GmxWdvzd/Screen-Shot-2019-05-14-at-11-05-34-PM.png'
+    title: 'University of Texas Institute for Geophysics',
+    stack: 'HTML, CSS, Wordpress',
+    deets: 'Professional project built to spec for UTIG',
+    goals: 'Incorporated an accessible content management system for department heads, incorporated SEO best practices and acted as webmaster while creating conteht',
+    image: 'https://i.postimg.cc/gjzdHBP0/Screen-Shot-2019-05-30-at-10-33-03-AM.png'
   }
 ]
