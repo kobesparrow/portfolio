7f2ee979922bb6e8a04d2bc73ce9958423901836
kobesparrow <markcollins.md@gmail.com>
2019-05-30 16:53:37 -0600

    Style project details

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Contact = () => {
+  
+  return(
+    <section>
+      <h2>CONTACT</h2>
+      <section className='contact-info'>
+        <h3>Mark Collins</h3>
+        <hr />
+      </section>
+    </section>
+  )
+}
+
+export default Contact;
