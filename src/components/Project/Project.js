9a9fcc0b9c3e219d9807f2739569b049ba56fba2
kobesparrow <markcollins.md@gmail.com>
2019-05-30 15:14:46 -0600

    Spacing styles

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -8,8 +8,10 @@ const Project = ({title, stack, deets, goals, image}) => {
       <section className='project-details'>
         <p className='project-title'>{ title }</p>
         <p className='tech-stack'><span className='project-subhead'>Tech Stack: </span>{ stack }</p>
-        <p>{ deets }</p>
-        <p>{ goals }</p>
+        <section className='project-specifics'>
+          <p>{ deets }</p>
+          <p>{ goals }</p>
+        </section>  
       </section>
     </article>
   )
