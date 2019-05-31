e0b9fd3c84bffdecf4b99227b10cc1b264656706
kobesparrow <markcollins.md@gmail.com>
2019-05-31 12:34:06 -0600

    Updates

---
+++ precache-manifest.be21117f4d1618efc4d070023522e098.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909ad2d4222f3be0c1f99146f13d1084",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "97df9de1711dd05bb637",
+    "url": "/portfolio/static/css/main.e849d5a4.chunk.css"
+  },
+  {
+    "revision": "188f6cf5aa84b36811bc",
+    "url": "/portfolio/static/js/2.54d03290.chunk.js"
+  },
+  {
+    "revision": "97df9de1711dd05bb637",
+    "url": "/portfolio/static/js/main.8066e78b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "bddaf9b1c54cc38bc938b015dc3d8a8f",
+    "url": "/portfolio/static/media/IMG_2284.bddaf9b1.JPG"
+  },
+  {
+    "revision": "3b078490dea1dca906964dec8c5614b1",
+    "url": "/portfolio/static/media/chevron.3b078490.svg"
+  },
+  {
+    "revision": "03ef488d95902be2cdd6b0b740a233df",
+    "url": "/portfolio/static/media/github.03ef488d.svg"
+  },
+  {
+    "revision": "bb026c4d996a5d563f7fceb5166175e1",
+    "url": "/portfolio/static/media/instagram.bb026c4d.svg"
+  },
+  {
+    "revision": "f5409d562d5d613246fafbd03fe6a857",
+    "url": "/portfolio/static/media/linkedin.f5409d56.svg"
+  },
+  {
+    "revision": "4fc7e57fcc044d3dd315139fe7eb5260",
+    "url": "/portfolio/static/media/mark-profile.4fc7e57f.jpeg"
+  },
+  {
+    "revision": "f2201e4f6b70740278cc8fb72eb29470",
+    "url": "/portfolio/static/media/stephen-walker-1160866-unsplash.f2201e4f.jpg"
+  }
+]);
