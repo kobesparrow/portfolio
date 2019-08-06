d2d09a1d7c680853e234ab7a9386e1c8fd81d7c0
kobesparrow <markcollins.md@gmail.com>
2019-08-06 12:44:51 -0600

    Updates

---
+++ precache-manifest.d1c0197b42f24e144854a3e2f1880593.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "819bfe2d780867707034311db8de2689",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a342461425e95f80aae7",
+    "url": "/static/css/main.a731f68a.chunk.css"
+  },
+  {
+    "revision": "09f5a1a6366118102e2f",
+    "url": "/static/js/2.ebd8ecfa.chunk.js"
+  },
+  {
+    "revision": "a342461425e95f80aae7",
+    "url": "/static/js/main.6661f0b4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bddaf9b1c54cc38bc938b015dc3d8a8f",
+    "url": "/static/media/IMG_2284.bddaf9b1.JPG"
+  },
+  {
+    "revision": "3b078490dea1dca906964dec8c5614b1",
+    "url": "/static/media/chevron.3b078490.svg"
+  },
+  {
+    "revision": "e4f2d6b028d93b8c7988d2132bf13ca2",
+    "url": "/static/media/github-hover.e4f2d6b0.svg"
+  },
+  {
+    "revision": "03ef488d95902be2cdd6b0b740a233df",
+    "url": "/static/media/github.03ef488d.svg"
+  },
+  {
+    "revision": "b1c58ee19d1e2c38b29ae86f3174f2e4",
+    "url": "/static/media/instagram-hover.b1c58ee1.svg"
+  },
+  {
+    "revision": "bb026c4d996a5d563f7fceb5166175e1",
+    "url": "/static/media/instagram.bb026c4d.svg"
+  },
+  {
+    "revision": "4b475fb3f54b3e35daa04295ec48df4c",
+    "url": "/static/media/linkedin-hover.4b475fb3.svg"
+  },
+  {
+    "revision": "f5409d562d5d613246fafbd03fe6a857",
+    "url": "/static/media/linkedin.f5409d56.svg"
+  },
+  {
+    "revision": "4fc7e57fcc044d3dd315139fe7eb5260",
+    "url": "/static/media/mark-profile.4fc7e57f.jpeg"
+  },
+  {
+    "revision": "df862f5a6886686848ddefefc26ee1f9",
+    "url": "/static/media/menu.df862f5a.svg"
+  },
+  {
+    "revision": "f2201e4f6b70740278cc8fb72eb29470",
+    "url": "/static/media/stephen-walker-1160866-unsplash.f2201e4f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1c4a11ca61612e9f9fa801d705efd8dc.js"
+  "/precache-manifest.d1c0197b42f24e144854a3e2f1880593.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
